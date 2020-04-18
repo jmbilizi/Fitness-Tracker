1eba676645b48d3e9e9b8f396628cc0b317a0d1f
Admin <admin@Admins-MacBook-Pro.local>
2020-04-18 16:55:44 -0600

    worked on api post, put, and get

--- routes/api.js
+++ routes/api.js
@@ -2,31 +2,45 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
-router.post("/api/transaction", ({ body }, res) => {
-  Workout.create(body)
-    .then((dbTransaction) => {
-      res.json(dbTransaction);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
-});
- 
-router.post("/api/transaction/bulk", ({ body }, res) => {
-  Workout.insertMany(body)
-    .then((dbTransaction) => {
-      res.json(dbTransaction);
+router.get("/api/workouts", (req, res) => {
+  Workout.find({})
+    .sort({ date: -1 })
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
     })
     .catch((err) => {
       res.status(400).json(err);
     });
 });
 
-router.get("/api/transaction", (req, res) => {
+router.get("/api/workouts/range", (req, res) => {
   Workout.find({})
-    .sort({ date: -1 })
-    .then((dbTransaction) => {
-      res.json(dbTransaction);
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.post("/api/workouts", (req, res) => {
+  Workout.create({})
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+  Workout.findByIdAndUpdate(
+    params.id,
+    { $push: { exercises: body } },
+    { new: true, runValidators: true }
+  )
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
     })
     .catch((err) => {
       res.status(400).json(err);
