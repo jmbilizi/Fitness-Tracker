ed5dfd9a02e86ac34432a68ecf8926d3df31c1d3
Admin <admin@Admins-MacBook-Pro.local>
2020-04-19 11:05:11 -0600

    fixed server.js file

--- server.js
+++ server.js
@@ -10,15 +10,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-  process.env.MONGODB_URI ||
-    "mongodb://admin:Janvier1995@ds157571.mlab.com:57571/workout",
-  {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-  }
-);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
 
+// routes
 app.use(require("./routes/api.js"));
 app.use(require("./routes/view.js"));
 
